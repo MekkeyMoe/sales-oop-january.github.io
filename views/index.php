07ecb5f38102fabe84191c3421d08079b5ac9084
Moe Meki <moeinatani@yahoo.co.jp>
2024-01-15 20:00:09 +0900

    first upload

---
+++ views/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Login</title>
+</head>
+<body>
+    <div style="height: 100vh;">
+        <div class="row h-100 m-0">
+            <div class="card w-25 my-auto mx-auto">
+                <div class="card-header bg-white border-0 py-3">
+                    <h1 class="text-center">LOGIN</h1>
+                </div>
+                <div class="card-body">
+                    <form action="../actions/login.php" method="POST">
+                        <input type="text" name="username" id="username" class="form-control mb-2" placeholder="USERNAME">
+
+                        <input type="password" name="password" id="password" placeholder="PASSWORD" class="form-control mb-5">
+
+                        <button type="submit" class="btn btn-primary w-100">Log in</button>
+                    </form>
+
+                    <p class="text-center mt-3 small"><a href="register.php">Create Account</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
